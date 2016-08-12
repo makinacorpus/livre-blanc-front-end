8d8942dc965fa49448905a4e06d2ba7269d7a534
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-08-12 18:40:44 +0200

    Ajout d'un spinner sur les boutons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
 var API_URL = 'http://127.0.0.1:5000';
 // var API_URL = 'https://mails-livre-blanc.herokuapp.com';
 
+// Create a new instance of ladda for the specified button
+var buttonD = window.Ladda.create(document.getElementById('download-button'));
+var buttonN = window.Ladda.create(document.getElementById('newsletter-button'));
+
 function showResponse(element, type, text) {
   document.getElementById(element).innerHTML = text;
   document.getElementById(element).classList = 'form-response ' + type;
 }
 
 function download(e) {
+  e.preventDefault(e);
+  buttonD.start();
   window.ga('send', {
     hitType: 'event',
     eventCategory: 'Button',
@@ -14,12 +20,12 @@ function download(e) {
     eventLabel: 'Téléchargement livre blanc'
   });
 
-  e.preventDefault(e);
   var email = e.target[0].value;
   var url = API_URL + '/download?email=' + email;
 
   fetch(url)
   .then(function(response) {
+    buttonD.stop();
     if (response.status !== 200) {
       showResponse('download-response', 'error', 'Une erreur est survenue. Veuillez réessayer. Status Code: ' + response.status);
       return;
@@ -30,11 +36,15 @@ function download(e) {
     });
   })
   .catch(function(error) {
+    buttonD.stop();
     showResponse('download-response', 'error', 'Erreur serveur : ' + error);
   });
 }
 
 function subscribe(e) {
+  e.preventDefault(e);
+  buttonN.start();
+
   window.ga('send', {
     hitType: 'event',
     eventCategory: 'Button',
@@ -42,12 +52,12 @@ function subscribe(e) {
     eventLabel: 'Inscription newsletter'
   });
 
-  e.preventDefault(e);
   var email = e.target[0].value;
   var url = API_URL + '/subscribe?email=' + email;
 
   fetch(url)
   .then(function(response) {
+    buttonN.stop();
     if (response.status !== 200) {
       showResponse('newsletter-response', 'error', 'Une erreur est survenue. Veuillez réessayer. Status Code: ' + response.status);
       return;
@@ -62,6 +72,7 @@ function subscribe(e) {
     });
   })
   .catch(function(error) {
+    buttonN.stop();
     showResponse('newsletter-response', 'error', 'Erreur serveur : ' + error);
   });
 }
