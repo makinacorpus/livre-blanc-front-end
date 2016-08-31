f45b2439ad55e5e1e0529de5fbd59883dca5aa69
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-08-31 10:36:50 +0200

    Ajout d'un formulaire

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,114 @@
+(function() {
+
+'use strict';
+
+
+/******************************************************
+API URLs
+******************************************************/
+
 var API_URL = 'http://127.0.0.1:5000';
 // var API_URL = 'https://mails-livre-blanc.herokuapp.com';
+
+
+/******************************************************
+Loading buttons
+******************************************************/
 
 // Create a new instance of ladda for the specified button
 var buttonD = window.Ladda.create(document.getElementById('download-button'));
 var buttonN = window.Ladda.create(document.getElementById('newsletter-button'));
+
+
+/******************************************************
+Form response
+******************************************************/
 
 function showResponse(element, type, text) {
   document.getElementById(element).innerHTML = text;
   document.getElementById(element).classList = 'form-response ' + type;
 }
 
+
+/******************************************************
+Animate the scroll
+******************************************************/
+
+var smoothScroll = function (anchor, duration) {
+
+    // Calculate how far and how fast to scroll
+    var startLocation = window.pageYOffset;
+    var endLocation = anchor.offsetTop;
+    var distance = endLocation - startLocation;
+    var increments = distance/(duration/16);
+    var stopAnimation;
+
+    // Scroll the page by an increment, and check if it's time to stop
+    var animateScroll = function () {
+        window.scrollBy(0, increments);
+        stopAnimation();
+    };
+
+    // If scrolling down
+    if ( increments >= 0 ) {
+        // Stop animation when you reach the anchor OR the bottom of the page
+        stopAnimation = function () {
+            var travelled = window.pageYOffset;
+            if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+                clearInterval(runAnimation);
+            }
+        };
+    }
+    // If scrolling up
+    else {
+        // Stop animation when you reach the anchor OR the top of the page
+        stopAnimation = function () {
+            var travelled = window.pageYOffset;
+            if ( travelled <= (endLocation || 0) ) {
+                clearInterval(runAnimation);
+            }
+        };
+    }
+
+    // Loop the animation function
+    var runAnimation = setInterval(animateScroll, 16);
+};
+
+
+/******************************************************
+Open form after enter email
+******************************************************/
+
+var email = '';
+
+function openForm(e) {
+    e.preventDefault(e);
+
+    var downloadSection = document.getElementById('download-section');
+    email = e.target[0].value;
+
+    downloadSection.classList.toggle('open');
+    smoothScroll(downloadSection, 500 || 500);
+}
+
+
+/******************************************************
+Download after completing form
+******************************************************/
+
 function download(e) {
   e.preventDefault(e);
+
+  var data = new FormData();
+  data.append('email', email);
+  for (var i = 0; i < e.target.length; i++) {
+    var el = e.target[i];
+
+    if (el.type === 'text') {
+      data.append(el.id, el.value);
+    }
+  }
+
   buttonD.start();
   window.ga('send', {
     hitType: 'event',
@@ -20,10 +117,12 @@ function download(e) {
     eventLabel: 'Téléchargement livre blanc'
   });
 
-  var email = e.target[0].value;
-  var url = API_URL + '/download?email=' + email;
+  var url = API_URL + '/download';
 
-  fetch(url)
+  fetch(url, {
+    method: 'POST',
+    body: data
+  })
   .then(function(response) {
     buttonD.stop();
     if (response.status !== 200) {
@@ -41,8 +140,14 @@ function download(e) {
   });
 }
 
+
+/******************************************************
+Subscribe newsletter
+******************************************************/
+
 function subscribe(e) {
   e.preventDefault(e);
+  buttonN.start();
   buttonN.start();
 
   window.ga('send', {
@@ -77,5 +182,9 @@ function subscribe(e) {
   });
 }
 
+document.getElementById('email').addEventListener('submit', openForm, false);
 document.getElementById('download').addEventListener('submit', download, false);
 document.getElementById('newsletter').addEventListener('submit', subscribe, false);
+
+
+})();
