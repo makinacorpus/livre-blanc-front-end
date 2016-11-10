3d7c0074992cceefc820c0f3676e57a553d2ef8f
Bastien Alvez <bastien.alvez@gmail.com>
2016-11-10 13:27:32 +0100

    New design merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,7 @@
 API URLs
 ******************************************************/
 
-<<<<<<< HEAD
-// var API_URL = 'http://127.0.0.1:5000';
-=======
 // var API_URL = 'http://127.0.0.1:4000';
->>>>>>> master
 var API_URL = 'https://mails-livre-blanc.herokuapp.com';
 
 
